b87ce8305810fb9c655c58f329f3b67db5144533
Hussein Elghareb <halghryb287@gmail.com>
2024-06-26 19:02:59 +0300

    intro to data types in Js

---
+++ 4-Datatypes/main.js
@@ -0,0 +1,22 @@
+/*
+    string
+    number
+    array => object
+    object
+    boolean
+    undefined
+    null
+*/
+
+console.log(typeof "Hussein");//string
+console.log(typeof 'Hussein');//string
+console.log(typeof 'H');//string
+console.log(typeof 2.9);//number
+console.log(typeof 2);//number
+console.log(typeof "2");//string
+console.log(typeof '2');//string
+console.log(typeof [10, 15, 17]);//object
+console.log(typeof { name: "Hussein", age: 21, country: "Egypt" });//object
+console.log(typeof true);//boolean
+console.log(typeof undefined);//undefined
+console.log(typeof null);//null
