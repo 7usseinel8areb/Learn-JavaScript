365adc552951e0bfd9ce1faa780dd05cc31b3330
Hussein Elghareb <halghryb287@gmail.com>
2024-06-27 00:55:12 +0300

    + - * / %

---
+++ 10-Arithmatic operators/main.js
@@ -0,0 +1,34 @@
+console.log(10 + 20);//30
+console.log(10 + "Hussein");//10Hussein
+
+console.log(10 - 20);//-10
+console.log(10 - "Hussein");//Nan
+
+console.log(typeof NaN);//Number
+
+console.log(10 * 20);//200
+console.log(10 * -20);//-200
+
+console.log(20 / 10);
+console.log(10 / 20);
+
+console.log(2 ** 4);
+
+console.log(10 % 2);
+console.log(11 % 3);
+
+let num = 1;
+console.log(num++);//1
+console.log(num);//2
+
+num = 1;
+console.log(++num);//2
+console.log(num)//2
+
+num = 1;
+console.log(num--);//1
+console.log(num);//0
+
+num = 1;
+console.log(--num);//0
+console.log(num)//0
