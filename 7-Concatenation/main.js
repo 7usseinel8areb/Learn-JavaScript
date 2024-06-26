a238f8562a6897025ac3747a10aafdd6cfae591d
Hussein Elghareb <halghryb287@gmail.com>
2024-06-26 20:11:22 +0300

    Concatination '+'

---
+++ 7-Concatenation/main.js
@@ -0,0 +1,18 @@
+let a = "Hssein";
+let b = "Mohamed";
+document.write(a + " " + b, '</br>');
+document.write(a," ", b,'</br>');
+
+let c = 2;
+document.write(c + b,'</br>');
+
+let d = '2';
+document.write(c + d);
+
+/*
+Output:
+    Hssein Mohamed
+    Hssein Mohamed
+    2Mohamed
+    22
+*/
