8958792f6ae20c4b997ec97461c6a46481353b7f
Hussein Elghareb <halghryb287@gmail.com>
2024-06-26 21:07:59 +0300

    Template strings writing

---
+++ 8-Template strings/main.js
@@ -0,0 +1,13 @@
+let title = "Hussein";
+let description = "Backend Developer";
+
+let markUp = `
+    <div class = "card">
+        <div class = "child">
+            <h1>${title}</h1>
+            <p>${description}</p>
+        </div>
+    </div>
+`;
+
+document.write(markUp); 
