1f2ac9cd313e4657d3a56640c4b9c016010b8617
Hussein Elghareb <halghryb287@gmail.com>
2024-06-26 16:23:57 +0300

    Output to screen

---
+++ 2-Output to screen/main.js
@@ -0,0 +1,13 @@
+/*
+1- window.alert()
+2- documnt.write()
+3- console.log()
+*/
+
+//Not a good choice
+window.alert("Hello from js file");
+
+document.write("<h1>Hello new page</h1>");
+//document.createElement("")//better than the previous
+
+console.log("Hello from js file");
