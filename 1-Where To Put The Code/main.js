669df773f89bc94416dfed3192c91daf93a70fb6
Hussein Elghareb <halghryb287@gmail.com>
2024-06-26 16:05:52 +0300

    Where to add script at HTML file

---
+++ 1-Where To Put The Code/main.js
@@ -0,0 +1,9 @@
+
+document.querySelector("h1").style.color = "Blue"; 
+// to avoid the null error
+
+//use onload event
+//This means after loading all the page make this
+window.onload = function () {
+    document.querySelector("h1").style.color = "Blue"; 
+}
