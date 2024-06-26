156cc64b1aa1bf8b886193b808cecbcbba3e2eeb
Hussein Elghareb <halghryb287@gmail.com>
2024-06-27 02:53:05 +0300

    Type casting

---
+++ 13-Type casting/main.js
@@ -0,0 +1,16 @@
+let a = "10";
+let b = 20;
+let c = true;
+
+console.log(a + b);
+console.log(+a + b);
+console.log(a + c);
+console.log(a + +c);
+console.log(b + c);
+
+console.log(a - b);
+console.log("" - 3);
+
+console.log(a + b + c);
+console.log(b + c + a);
+console.log(+a + b + c);
