ab56cd30ead2a96ca83d931135ed7945aff2d23f
Hussein Elghareb <halghryb287@gmail.com>
2024-06-26 18:32:22 +0300

    Console methods (log, error, table) web API styling console %c

---
+++ 3-Console Methods/main.js
@@ -0,0 +1,22 @@
+/*
+Console Methods 
+log 
+error
+table
+
+web api
+ -> the console is not related with JS it is related to web API
+
+styling console 
+    directive %c
+*/
+//give the output
+console.log("Log");
+//giv me an error at the console
+console.error("Error");
+//give the index and the item at thhis item
+console.table(["Hussein", "Mohamed", "Ali"]);
+
+//styling
+console.log("Hello from %cJS file", "color:red; font-size:40px");
+console.log("Hello from %cJS %cfile", "color:red; font-size:40px", "color:blue; font-size:30px");
