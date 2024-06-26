fe6dbcc7f15bd528e5b4f68f3f0da7c280b1f696
Hussein Elghareb <halghryb287@gmail.com>
2024-06-27 02:27:11 +0300

    Converting from string to number

---
+++ 11-Unary operators/main.js
@@ -0,0 +1,16 @@
+console.log(+100);//100
+console.log(+"100");//100
+console.log(30 + +"100");//130
+console.log(+"-100");//-100
+console.log(+"15.5");//15.5
+console.log(+null);//0
+console.log(+false);//0
+console.log(+true);//1
+console.log(+"Hussein");//Nan
+console.log(-100);
+console.log(-"100");
+console.log(-"-100");
+console.log(40 + +"-100");//-60
+console.log(+0xff);//255
+console.log(-0xff);//-255
+console.log(Number("100"))
