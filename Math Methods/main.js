2db404fade60b616d97945cde67739bdf8be1973
Hussein Elghareb <halghryb287@gmail.com>
2024-06-27 14:26:23 +0300

    important Math methods

---
+++ Math Methods/main.js
@@ -0,0 +1,17 @@
+console.log(Math.round(1.2));
+console.log(Math.round(1.5));
+
+console.log(Math.ceil(1.2));
+console.log(Math.ceil(1.5));
+
+console.log(Math.floor(1.2));
+console.log(Math.floor(1.5));
+
+console.log(Math.max(10, 20, -29, 100));
+console.log(Math.min(10, 20, -29, 100));
+
+console.log(Math.pow(2, 4));
+
+console.log(Math.random());
+
+console.log(Math.trunc(99.5));
