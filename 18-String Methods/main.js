2dcbbc3c6c8eac2d5c890bc28314019bb02b461c
Hussein Elghareb <halghryb287@gmail.com>
2024-06-28 01:18:20 +0300

    String Methods

--- 18-String Methods/main.js
+++ 18-String Methods/main.js
@@ -13,3 +13,47 @@ console.log(theName.toLowerCase());
 console.log(theName.toUpperCase());
 
 console.log(theName.trim().charAt(0).toLowerCase());//h
+
+//-----------------------------------------------------------------
+
+let stringMethod2 = "Elzero web school";
+
+console.log(stringMethod2.indexOf("web"));
+console.log(stringMethod2.indexOf("web", 8));
+
+//start from the last index
+console.log(stringMethod2.lastIndexOf("o"));//15 -> Elzer[o]
+
+console.log(stringMethod2.slice(5, 10));//o web
+console.log(stringMethod2.slice(4));//ro web school
+console.log(stringMethod2.slice(-3));//ool
+console.log(stringMethod2.slice(-5, -3));//ch
+console.log(stringMethod2.repeat(5));
+
+console.log(stringMethod2.split());
+console.log(stringMethod2.split(' '));
+console.log(stringMethod2.split(''));
+console.log(stringMethod2.split(' ', 2));//limit
+
+
+//-----------------------------------------------------------------
+
+console.log(stringMethod2.substring(5));
+console.log(stringMethod2.substring(5, 8));
+console.log(stringMethod2.substring(-10));//it also will start from 0
+console.log(stringMethod2.substring(18));//Undefined
+console.log(stringMethod2.substring(stringMethod2.length - 1));
+
+console.log(stringMethod2.substr(1));
+console.log(stringMethod2.substr(-3));
+console.log(stringMethod2.substr(-5, 3));//this means start from the last 5 and then give me 3 chars -> cho
+
+
+console.log(stringMethod2.endsWith('l'));
+console.log(stringMethod2.endsWith('o',5));//the length not the index
+
+console.log(stringMethod2.startsWith('E'));
+console.log(stringMethod2.startsWith('W'));
+
+console.log(stringMethod2.includes('w', 2));//starting from index 2
+
