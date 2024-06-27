06ba2e06d4f50b0090fd56ec2c50e658424d1771
Hussein Elghareb <halghryb287@gmail.com>
2024-06-28 02:18:16 +0300

    Comparison operators

---
+++ 20-Comparison operators/main.js
@@ -0,0 +1,28 @@
+/*
+==
+!=
+
+===
+!==
+
+>=
+<=
+
+>
+<
+
+*/
+
+console.log(10 == "10");
+console.log(10 === "10");
+console.log(10 === 10);
+
+//-------------------------
+console.log(10 >= "10");
+console.log(10 >= 10);
+
+
+
+//-------------------------
+console.log("Osama" === "Ahmed");
+console.log(typeof "Osama" === typeof "Ahmed");
