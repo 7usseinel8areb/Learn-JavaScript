71571a3d3888f67469822d73dadda789c7acec38
Hussein Elghareb <halghryb287@gmail.com>
2024-06-27 17:21:50 +0300

    string methods

---
+++ 18-String Methods/main.js
@@ -0,0 +1,15 @@
+let theName = "  Hussein    ";
+console.log(theName[3]);
+
+console.log(theName.charAt(4));
+
+console.log(theName.length);
+
+console.log(theName.trimEnd());
+console.log(theName.trimStart()+"<- space here");
+console.log(theName.trim() + "<- no space here");
+
+console.log(theName.toLowerCase());
+console.log(theName.toUpperCase());
+
+console.log(theName.trim().charAt(0).toLowerCase());//h
