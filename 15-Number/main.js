794099b9c426c8e8e3d6d5cb4c3c343eed741d97
Hussein Elghareb <halghryb287@gmail.com>
2024-06-27 13:43:56 +0300

    Number

---
+++ 15-Number/main.js
@@ -0,0 +1,21 @@
+/*
+    Number
+        Douple Precision
+        Syntating Sugar "_"
+        e
+        **
+        With Decimal
+        Number + Bigint
+        Number Min and Max Values
+*/
+
+console.log(1000000);
+console.log(1_000_000);
+console.log(1e6);
+console.log(10 ** 6);
+console.log(1000000.000);
+console.log("------------------------------")
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.MIN_VALUE);
