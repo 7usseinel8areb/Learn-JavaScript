dd45d14c21ac097f21cd56961abee0b2a5657bb4
Hussein Elghareb <halghryb287@gmail.com>
2024-07-01 01:30:05 +0300

    edit challenge solution

--- 35-Challenge10/main.js
+++ 35-Challenge10/main.js
@@ -5,6 +5,6 @@ console.log(names("Hussein", "Ali", "shimaa"));
 
 
 let myNumbers = [20, 50, 10, 60];
-let calc = (one, two, ...nums) => myNumbers.reduce((past, current) => past + current, 0) - one - two;
+let calc = (one, two, ...nums) => nums.reduce((past, current) => past + current, 0) - one - two;
 
-console.log(calc(10,50,myNumbers))
+console.log(calc(10, 50, ...myNumbers));
