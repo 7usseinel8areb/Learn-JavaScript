adb416d40f08552c73566b6fb099bf1fbd065fa7
Hussein Elghareb <halghryb287@gmail.com>
2024-06-30 21:00:27 +0300

    Function parameters

--- 30-Functions/main.js
+++ 30-Functions/main.js
@@ -45,3 +45,35 @@ function generate(num1, num2) {
 
 result = generate(10, 20);
 console.log(result);
+
+
+//-------------------------------------------------------------------------------
+/*
+    Function
+        Default function parameters
+        Old strategic[Condition+ logical Or]
+        ES6 Method
+*/
+
+//old
+function printHello(userName,age) {
+    if (age === null) {
+        age = "Unkown";
+    }
+    console.log(`Hello ${userName} your age is ${age}`);
+}
+
+function printHello(userName,age) {
+    age = age || "Unkown";
+    console.log(`Hello ${userName} your age is ${age}`);
+}
+
+printHello("Mai")
+
+//ES6
+function printHello(userName,age = "Unkown") {
+    
+    console.log(`Hello ${userName} your age is ${age}`);
+}
+
+printHello("Hussein");
