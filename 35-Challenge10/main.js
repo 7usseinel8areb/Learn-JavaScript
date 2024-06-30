402f4cc6f2e8b997ba58e4b26e2fdfd6fc86964e
Hussein Elghareb <halghryb287@gmail.com>
2024-07-01 00:57:43 +0300

    Arrow function challenge

---
+++ 35-Challenge10/main.js
@@ -0,0 +1,10 @@
+
+let names = (...stringArray) => `String ${stringArray.map(e => `[${e}]`)} => Done !`
+
+console.log(names("Hussein", "Ali", "shimaa"));
+
+
+let myNumbers = [20, 50, 10, 60];
+let calc = (one, two, ...nums) => myNumbers.reduce((past, current) => past + current, 0) - one - two;
+
+console.log(calc(10,50,myNumbers))
