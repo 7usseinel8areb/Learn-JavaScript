57b27822102c4bf212cf3f774de2b840f25a6db8
Hussein Elghareb <halghryb287@gmail.com>
2024-06-30 20:48:59 +0300

    return & Interubting

--- 30-Functions/main.js
+++ 30-Functions/main.js
@@ -17,3 +17,31 @@ function sayHello(userName, age) {
 sayHello('Hossam',40);
 sayHello('Hussein',21);
 sayHello('Malak', 18);
+
+
+
+//------------------------------------------------------------------------------
+/*
+    Function
+        Return
+        Interruptting
+*/
+function print(userName) {
+    return `Hello ${userName}`;
+}
+
+let result = print('Hussin');
+console.log(result);
+
+
+function generate(num1, num2) {
+    for (let i = num1; i <=num2 ; i++) {
+        console.log(i);
+        if (i === 15) {
+            return 'Interubted';
+        }
+    }
+}
+
+result = generate(10, 20);
+console.log(result);
