da06ae7eea80b64b290fcfde93e25563e5ef2ccf
Hussein Elghareb <halghryb287@gmail.com>
2024-07-03 00:50:16 +0300

    Filter & practice on it

--- 36-Higher Order Function/main.js
+++ 36-Higher Order Function/main.js
@@ -58,3 +58,59 @@ let ignoreans = ignoreNumbers.split('').map((element) => {
 }).join('');
 
 console.log(ignoreNumbers, '=>', ignoreans);
+
+//------------------------------------------------------------------------------------------------------------------
+/*
+    Filter
+        method creates a new array 
+        with all elements that passes the test
+
+        syntax
+            filter(callBackfunction(element, index, array){},thisArg)
+*/
+
+let firends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
+//Get names starts with 'A'
+
+let a = firends.filter((element) => {
+    return element.startsWith('A'); 
+})
+
+a = firends.filter(e => e.startsWith('A'))
+
+console.log(a);
+
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//Get even numbers only
+
+let ans = numbers.filter(n => !(n % 2));
+console.log(ans);
+
+//practice
+/*
+    Filter
+        Filter longest word by number
+*/
+
+let scentence = "I love foood code too playing much";
+//filter words more than 4 chars
+ans = scentence.split(' ').filter(e => e.length <= 4).join(' ');
+console.log(ans)
+
+
+
+ignoreNumbers = "elz123er4o";
+//filter numbers
+ans = ignoreNumbers.split('')
+        .filter(element => isNaN(parseInt(element)))
+        .join('')
+console.log(ans);
+
+
+let mix = "a13BS2ZX";
+ans = mix.split('')
+        .filter(x => !isNaN(x))
+        .map(x => x * x);
+
+console.log(ans)
