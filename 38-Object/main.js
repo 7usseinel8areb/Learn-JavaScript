eac7f3b4d6884039c3145b75f7c250890c8b99c5
Hussein Elghareb <halghryb287@gmail.com>
2024-07-04 00:46:17 +0300

    Create an object with assign method

--- 38-Object/main.js
+++ 38-Object/main.js
@@ -183,3 +183,34 @@ user = {
 console.log(user.checkWork());
 user.available = true;
 console.log(user.checkWork());
+
+//--------------------------------------------------------------------------------------------------------------------
+/*
+    Object
+        Create object with assign method
+*/
+let obj1 = {
+    prop1: 1,
+    meth1: function () {
+        return this.prop1
+    }
+}
+let obj2 = {
+    prop2: 2,
+    meth2: function () {
+        return this.prop2
+    }
+}
+
+let targetObject = {
+    prop1: 100,
+    prop3: 3
+}
+
+// لو في اكتر من ميثود او بروبرتي بياخد اول واحده
+let finalObject = Object.assign(targetObject, obj1, obj2);
+console.log(finalObject);
+
+let newObject = Object.assign({}, obj1, { prop: 5, prop6: 100 });
+
+console.log(newObject);
