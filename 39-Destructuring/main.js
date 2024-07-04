9d4fb06c206b0ef77e88c70557390ec2ce81130d
Hussein Elghareb <halghryb287@gmail.com>
2024-07-04 14:47:59 +0300

    Destructuring arrays

---
+++ 39-Destructuring/main.js
@@ -0,0 +1,19 @@
+/*
+    Destructuring
+        -> is ajs expression that allows us to extract data from arrays, objects then map and set them into new variables 
+*/
+let myFriends = ["Ahmed", "Sayed", "Hussein", "Ali"];
+
+// //Destructuring
+// let [a, b, c, d ,e] = myFriends;
+
+// console.log(c);//Hussein
+// console.log(e)//undefinned
+
+//to solve this put a default values
+[a, b, c, d, e = "Kareem"] = myFriends;
+console.log(e);// Kareem but we can't override at line 8 so we can't define kareem make comment for line 8
+
+//if i want 1 , 2 , 4
+let [x, y, , z] = myFriends;
+console.log(z);//Ali
