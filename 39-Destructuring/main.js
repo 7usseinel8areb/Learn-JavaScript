cb2b4515689a7f2daa40d40b127b433dc3d1a7fe
Hussein Elghareb <halghryb287@gmail.com>
2024-07-06 19:46:48 +0300

    Remove Challenge from Destructuring folder to Challenge12 folder

--- 39-Destructuring/main.js
+++ 39-Destructuring/main.js
@@ -188,23 +188,3 @@ console.log(aa);
 console.log(j);
 console.log(p);
 console.log(eg);
-
-//----------------------------------------------------------------------------------------------------------------------------------
-/*
-    Challenge
-*/
-
-let chosen = 1;
-
-myFriends = [
-    { title: "Osama", age: 30, available: true, skills: ["HTML", "CSS"] },
-    { title: "Hussein", age: 20, available: false, skills: ["C#", "JS"] },
-    { title: "AKmal", age: 45, available: true, skills: ["Python", "C++"] }
-];
-
-({ title: n, age: a, available, availablility: av = available ? "Available" : "Not Available", skills: [firstSkill] } = myFriends[chosen - 1]);
-console.log(n);
-console.log(a);
-console.log(n);
-console.log(av);
-console.log(firstSkill);
