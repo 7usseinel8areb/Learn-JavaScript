c646d37a5bd1200fe3938cf2dccc8b9d5bcc809e
Hussein Elghareb <halghryb287@gmail.com>
2024-07-08 14:20:49 +0300

    Modifires

--- 46-Regular expression/main.js
+++ 46-Regular expression/main.js
@@ -18,3 +18,46 @@ url = 'https://www.elzero.com';
 url = 'https://.elzero.com';
 url = 'elzero.org'
 url = 'elzero.org/'
+
+
+
+//---------------------------------------------------------------------------------------------------------------
+/*
+    Regular Expression
+
+    syntax
+        /pattern/modifier(s)
+        new RegExp("pattern", "modifires")
+
+    Search Method
+        match(pattern)
+
+    Modifiers
+        i => case sensetive
+        g => global
+        m => multilines => sez=arch in multi lines
+*/
+
+let myString = "Hello Elzero web school I love elzero";
+
+//Search
+let regex = /Elzero/;
+console.log(myString.match(regex));//['Elzero', index: 6, input: 'Hello Elzero web school I love elzero', groups: undefined]
+regex = /elzero/;
+console.log(myString.match(regex));//['elzero', index: 31, input: 'Hello Elzero web school I love elzero', groups: undefined]
+
+console.log("####".repeat(4));
+
+//Modifires
+//i => case sensetive
+regex = /elzero/i;
+console.log(myString.match(regex));//['Elzero', index: 6, input: 'Hello Elzero web school I love elzero', groups: undefined]
+
+//g => global
+//give me all valid matching words
+regex = /elzero/ig;
+console.log(myString.match(regex));//['Elzero', 'elzero']
+regex = /elzero/g;
+console.log(myString.match(regex));//['elzero']
+regex = /elze/ig;
+console.log(myString.match(regex));//['Elze', 'elze']
