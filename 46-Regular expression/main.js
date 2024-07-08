fb981a1f11a03201a17dc73b2938e25e598d4903
Hussein Elghareb <halghryb287@gmail.com>
2024-07-08 15:42:59 +0300

    Regular expression ranges part1

--- 46-Regular expression/main.js
+++ 46-Regular expression/main.js
@@ -61,3 +61,49 @@ regex = /elzero/g;
 console.log(myString.match(regex));//['elzero']
 regex = /elze/ig;
 console.log(myString.match(regex));//['Elze', 'elze']
+
+//---------------------------------------------------------------------------------------------------------------
+/*
+    Regular Expression
+        part1
+            1) (X|Y) => X or Y
+            2) [0-9] => 0 to 9
+            3) [^0-9] => Any charachter Not 0 to 9
+*/
+
+console.log("####".repeat(10));
+
+let tld = "Com Net Org Info Code Io";
+let tldRegEx = /(Net|Com|Io)/i;
+console.log(tld.match(tldRegEx));//find first of any of the regex
+
+tld = "Com Net Org Info Code Io";
+tldRegEx = /(Net|Com|Io)/ig;
+console.log(tld.match(tldRegEx));// ['Com', 'Net', 'Io']
+
+console.log("####".repeat(4));
+
+let nums = "12345678910";
+let numsRegEx = /[0-9]/g
+console.log(nums.match(numsRegEx))//Any number
+
+nums = "-1-2";
+console.log(nums.match(numsRegEx))//[1, 2]\
+
+nums = "12345678910";
+numsRegEx = /[1-3]/g;
+console.log(nums.match(numsRegEx));//['1', '2', '3', '1']
+
+console.log("####".repeat(4));
+
+let notNums = "12345678910Abcs@/\\";
+let notNumsRegEx = /[^0-9]/g;
+console.log(notNums.match(notNumsRegEx));//['A', 'b', 'c', 's', '@', '/', '\\']
+
+console.log("####".repeat(4));
+
+//Need start with Os and then have a number from 5 to 9 and end with Os
+let practice = "Os1 Os1Os Os2 Os8 Os8Os";
+let practiceRegEx = /Os[5-9]Os/ig;
+console.log(practice.match(practiceRegEx));//['Os8Os']
+
